4e2234ec4f03ed1165afca02e9c38cb10ff62fae
Willian-Charantola <linguasindigenasms@gmail.com>
2025-08-21 22:43:49 -0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "../src/Pages/Home"
+import './App.css'
+
+function App() {
+  
+  return (
+   
+     
+
+       <Router basename="/linguas_indigenas/">
+     
+      <div className="pt-20">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home/>
+              
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+   
+  )
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+
+
+
+const Home = () => {
+
+
+    <main>
+
+
+        <div style={{background : "#fff" }}>
+
+            Hello world!!!
+        </div>
+    </main>
+}
+
+export default Home; 
